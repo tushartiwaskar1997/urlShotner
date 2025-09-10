74be6b9c20997a320b5b5a0d158adde9b4b0cd22
tushartiwaskar1997 <tushartiwaskar.svpcet@gmail.com>
2025-09-10 16:58:02 +0530

    spring base file

---
+++ src/main/java/com/example/url_shotner/UrlShorterApplication.java
@@ -0,0 +1,13 @@
+package com.example.url_shotner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShorterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShorterApplication.class, args);
+	}
+
+}
